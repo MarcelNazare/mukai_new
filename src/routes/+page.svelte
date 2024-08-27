dc5799ae8bc98f016d81ab0d607ebf44b947598e
Marcel Nazare <realmarcelnazare@gmail.com>
2024-08-27 22:48:34 +0200

    added postcss line to the page.svelte file

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,7 @@
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 
-<style>
+<style lang="postcss">
     :global(html){
         background-color: theme(colors.gray.100);
     }
